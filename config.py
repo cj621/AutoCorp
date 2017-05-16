88a7b3b5b57dc9796f7a03d7a8099e040ed8ceb2
Chaitanya Jadhav <cjadhav7477@gmail.com>
2017-05-16 17:31:28 +0530

    Project Setup

---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:chitts@localhost/autocorp'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'Thisisasecret!'
---
+++ views/routes.py
@@ -0,0 +1,10 @@
+from main import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("login.html")
+
+@app.route('/login')
+def login():
+	return "Login Route"
